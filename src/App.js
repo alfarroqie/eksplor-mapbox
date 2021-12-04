5b2dedfdf6c3220f7b7008885f6b47bd9e91597a
alfarroqie <chofief.al.tif418@polban.ac.id>
2021-12-04 13:19:41 +0700

    add geojson marker

--- src/App.js
+++ src/App.js
@@ -8,43 +8,84 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hvY29vcmVvIiwiYSI6ImNrdDgxZG5ibzB4dGkycGxqZ
 export default function MapDataDownload() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-87.661557);
-  const [lat, setLat] = useState(41.893748);
-  const [zoom, setZoom] = useState(10);
+  const [lng, setLng] = useState(-96);
+  const [lat, setLat] = useState(37.8);
+  const [zoom, setZoom] = useState(2);
   //INIT MAP
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
     container: mapContainer.current,
-    style: 'mapbox://styles/examples/cjgiiz9ck002j2ss5zur1vjji',
+    style: 'mapbox://styles/mapbox/light-v10',
     center: [lng, lat],
     zoom: zoom
     });
   });
 
-  //POPUP
+  //
   useEffect(() => {
     if(!map.current) return;
-    map.current.on('click', 'chicago-parks', (e) => {
-      const coordinate = e.features[0].geometry.coordinates
-      const title = e.features[0].properties.title
-      const desc = e.features[0].properties.description
-      
-      new mapboxgl.Popup()
-      .setLngLat(coordinate).setHTML(
-        `<h3>${title}</h3>
-        <p>${desc}</p>
-        `
-      ).addTo(map.current);
+    map.current.on('load', () => {
+      // Add an image to use as a custom marker
+      map.current.loadImage(
+        'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
+        (error, image) => {
+        if (error) throw error;
+        map.current.addImage('custom-marker', image);
+        // Add a GeoJSON source with 2 points
+        map.current.addSource('points', {
+          'type': 'geojson',
+          'data': {
+          'type': 'FeatureCollection',
+          'features': [
+          {
+          // feature for Mapbox DC
+          'type': 'Feature',
+          'geometry': {
+          'type': 'Point',
+          'coordinates': [
+          -77.03238901390978, 38.913188059745586
+          ]
+          },
+          'properties': {
+          'title': 'Mapbox DC'
+          }
+          },
+          {
+          // feature for Mapbox SF
+          'type': 'Feature',
+          'geometry': {
+          'type': 'Point',
+          'coordinates': [-122.414, 37.776]
+          },
+          'properties': {
+          'title': 'Mapbox SF'
+          }
+          }
+          ]
+          }
+        });
+        
+        // Add a symbol layer
+        map.current.addLayer({
+          'id': 'points',
+          'type': 'symbol',
+          'source': 'points',
+          'layout': {
+          'icon-image': 'custom-marker',
+          // get the title name from the source's "title" property
+          'text-field': ['get', 'title'],
+          'text-font': [
+          'Open Sans Semibold',
+          'Arial Unicode MS Bold'
+          ],
+          'text-offset': [0, 1.25],
+          'text-anchor': 'top'
+          }
+        });
+        }
+      );
     });
-    map.current.on('mouseenter', 'chicago-parks', () => {
-      map.current.getCanvas().style.cursor = 'pointer';
-      });
-       
-      // Change it back to a pointer when it leaves.
-      map.current.on('mouseleave', 'chicago-parks', () => {
-      map.current.getCanvas().style.cursor = '';
-      });
   })
   
   return (
